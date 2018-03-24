dfbc43b1ff8e32af8b697c1368f8b8a8bdc817da
wahyudhiani <32408890+wahyudhiani@users.noreply.github.com>
2018-03-24 15:58:04 +0700

    Membuat Atribut dan Behavior

--- src/praktek01/Buku.java
+++ src/praktek01/Buku.java
@@ -10,5 +10,19 @@ package praktek01;
  * @author wahyudiani
  */
 public class Buku {
+    Integer ukuran;
+    String bentuk;
+    String warna;
+    Integer jumlahisi;
+    String kegunaan;
+    
+    void caramenyimpan(){
+        System.out.println("Ukuran    :"+ukuran);
+        System.out.println("Bentuk    :"+bentuk);
+        System.out.println("Warna     :"+warna);
+        System.out.println("Jumlahisi :"+jumlahisi);
+        System.out.println("Kegunaan  :"+kegunaan);
+    }
+    
     
 }
